c31e3b49bb0cd2d499499c187d9bdfd4a3d5a8e6
zezuev <sergej.zuev@icloud.com>
2024-07-25 05:43:00 +0200

    Project setup

---
+++ src/main/java/com/example/streamingapp/StreamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingAppApplication.class, args);
+    }
+
+}
